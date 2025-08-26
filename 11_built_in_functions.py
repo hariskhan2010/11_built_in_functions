c63ff1b7b4ae6754f4f501a10256bc458c509914
hariskhan2010 <harismalik2010afridi@gmail.com>
2025-08-26 11:12:51 +0500

    Add files via upload

---
+++ 11_built_in_functions.py
@@ -0,0 +1,11 @@
+import os
+
+# create_folder = os.mkdir("HarisMalik1")
+# change_folder = os.chdir("HarisMalik")
+# current_folder = os.getcwd()
+# print(current_folder)
+
+create_folder = os.mkdir("pakistani")
+change_folder = os.chdir("pakistani1")
+current_folder = os.getcwd()
+print(current_folder)
